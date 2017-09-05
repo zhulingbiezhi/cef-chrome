3a1067de5da82cbc90697e7727573f8b193f0ecc
ququ <1577826189@qq.com>
2017-09-05 15:50:31 +0800

    删除cef debug版的依赖，使用release版本进行debug

--- make_copy_file.bat
+++ make_copy_file.bat
@@ -1,5 +1,5 @@
 xcopy "./dep/cef/bin/Release" "./QCefViewSDK/bin/release_x86" /s /e /y /i
 xcopy "./dep/cef/Resources" "./QCefViewSDK/bin/release_x86/resources" /s /e /y /i
-xcopy "./dep/cef/bin/Debug" "./QCefViewSDK/bin/debug_x86" /s /e /y /i
+xcopy "./dep/cef/bin/Release" "./QCefViewSDK/bin/debug_x86" /s /e /y /i
 xcopy "./dep/cef/Resources" "./QCefViewSDK/bin/debug_x86/resources" /s /e /y /i
 pause
